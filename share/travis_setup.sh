2cfc9a13935ac964f1ea13b5728348ae171fbfba
LlamaOnDrugs <47127170+LlamaOnDrugs@users.noreply.github.com>
2019-05-06 01:34:45 +0100

    Update travis_setup.sh

--- share/travis_setup.sh
+++ share/travis_setup.sh
@@ -5,5 +5,5 @@ mkdir ~/.quantisnetcore
 
 # safety check
 if [ ! -f ~/.quantisnetcore/.quantisnet.conf ]; then
-  cp share/quantisnet.conf.example ~/.quantisnetcore/dash.conf
+  cp share/quantisnet.conf.example ~/.quantisnetcore/quantisnet.conf
 fi
